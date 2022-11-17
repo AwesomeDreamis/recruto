1fb77b057d05fda9f2859985a5f4938a1565e7a2
Alexander Mokritsky <alexmdream@gmail.com>
2022-11-18 00:27:29 +0300

    Update recruto.py

--- recruto.py
+++ recruto.py
@@ -7,7 +7,3 @@ app = FastAPI()
 @app.get('/url_name/')
 def hello(name: str = 'name', message: str = 'message'):
     return f'Hello {name}! {message}!'
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
